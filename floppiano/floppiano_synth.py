ae8f37c10390c3d1bb41b2b1c77231558795056c
Jorian <joriankhan@gmail.com>
2024-06-18 15:47:29 -0400

    fixed floppiano Synth

--- floppiano/floppiano_synth.py
+++ floppiano/floppiano_synth.py
@@ -9,16 +9,16 @@ class FlopPianoSynth(KeyboardSynth):
     """
 
     def __init__(
-            self,
-            voices: tuple[DriveVoice],
-            keyboard: Keyboard, 
-            crash_mode:DriveVoice.CrashMode = DriveVoice.CrashMode.FLIP,
-            spin:bool = False,        
-            **kwargs) -> None:
+        self,
+        voices: tuple[DriveVoice],
+        keyboard:Keyboard,
+        crash_mode:DriveVoice.CrashMode = DriveVoice.CrashMode.FLIP,
+        spin:bool = False,        
+        **kwargs) -> None:
 
         super().__init__(voices, keyboard, **kwargs)
-
-        self.logger.name = "FlopPianoSynth"
+        
+        self.logger.name = "DriveSynth"
         # Add support for crash mode and spin
         if 'crash_mode' not in self.control_change_map:
             self.control_change_map['crash_mode'] = 70
@@ -30,36 +30,30 @@ class FlopPianoSynth(KeyboardSynth):
         self.spin = spin
 
 
+    #-------------------------Overrides from Synth-----------------------------#
 
     # overridden to update drive spin and crash modes
     def _sound(self) -> None:
-        try:
-            for voice in self._active_voices.values():
-                voice.crash_mode = self.crash_mode
-                voice.spin = self.spin
-                voice.pitch_bend(self.pitch_bend, self.pitch_bend_range)
-                voice.modulate(self.modulation, self.modulation_wave)
-                voice.update((not self._muted))
-        except BusException as be:
-            self.logger.warning("voices failed to update")   
-    
+        for voice in self._active_voices.values():
+            voice.crash_mode = self.crash_mode
+            voice.spin = self.spin
+            voice.pitch_bend(self.pitch_bend, self.pitch_bend_range)
+            voice.modulate(self.modulation, self.modulation_wave)
+            voice.update((not self._muted))
 
-    #------------------Getters/Setters-----------------------------------------#
+
+    #------------------------------Properties----------------------------------#
 
     @property
     def crash_mode(self)->DriveVoice.CrashMode:
         return self._crash_mode
-    
+
     @crash_mode.setter
     def crash_mode(self, crash_mode:int) -> None:
         if crash_mode not in DriveVoice.CrashMode.__members__.values():
-            self.logger.warning(
-                f"crash_mode NOT set: {crash_mode} is not a valid mode")
-            return
+            raise ValueError('Not a valid CrashMode')
         self._crash_mode = crash_mode    
-        self.logger.info(
-            'crash_mode set: '
-            f'{DriveVoice.CrashMode._member_names_[crash_mode]}')
+
 
     @property
     def spin(self):
@@ -68,5 +62,3 @@ class FlopPianoSynth(KeyboardSynth):
     @spin.setter
     def spin(self, spin:bool):
         self._spin = bool(spin)
-        self.logger.info(f'spin set: {self.spin}')
-
