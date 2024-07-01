b9f91cd839ad3b9eee993012595311d6f39f607b
Jorian <joriankhan@gmail.com>
2024-06-30 20:45:40 -0400

    more firmware changes

--- floppiano/new_drive_firm.py
+++ floppiano/new_drive_firm.py
@@ -1,30 +1,9 @@
 from jidi import bus
 from enum import IntEnum
 import struct
+import time
 
 class Drive ():
-    class CrashMode (IntEnum):
-        """
-        A Enum to hold Floppy drive crash modes
-
-        *  Crash mode - the two crash mode bits represent which crash mode the
-        *  drive will use. Two bit combined give four possible states:
-        *    MSB LSB
-        *     0   0  - Crash prevention OFF
-        *     0   1  - Crash prevention ON (BOW Mode)
-        *     1   0  - Crash prevention ON (FLIP Mode)
-        *     1   1  - Not used (results in crash prevention mode off if set)
-        *     
-        *     BOW mode: The drive will step until end of travel then flip
-        *     direction (like a violin bow changing direction at the end of a
-        *     bow stroke)
-        *     
-        *     FLIP mode: THe drive will flip direction after every step/pulse
-        """
-        __order__ = 'OFF BOW FLIP'
-        OFF = 0#0b00
-        BOW = 1#0b01
-        FLIP = 2#0b10
 
     """
     A class that represents a floppy drive
@@ -36,18 +15,20 @@ class Drive ():
             address:int,
             enable:bool = False, 
             spin:bool = False,
-            crash_mode:CrashMode = CrashMode.FLIP,
+            flip:bool = True,
             mod_frequency:int = 0,
-            frequency:float = 440.0,) -> None:
+            mod_rate:int = 0,
+            frequency:float = 440.0) -> None:
 
  
         self.address = address
         
         self.enable = enable #Drive enable state
         self.spin = spin #Drive spin state
-        self.crash_mode = crash_mode #Crash prevention mode
+        self.flip = flip #Crash prevention mode
         self.mod_frequency = mod_frequency # number times per second to modulate
-        self.frequency = frequency
+        self.mod_rate = mod_rate
+        self.frequency = frequency #the frequency to be played
 
     @property
     def address(self) -> int:
@@ -82,15 +63,29 @@ class Drive ():
         self._spin = spin        
     
     @property
-    def crash_mode(self) -> CrashMode:
+    def flip(self) -> bool:
         return self._crash_mode
     
-    @crash_mode.setter
-    def crash_mode(self, crash_mode:int) -> None:
-        if crash_mode not in Drive.CrashMode.__members__.values():
-            raise ValueError('not a valid CrashMode')
-        self._crash_mode = crash_mode  
+    @flip.setter
+    def flip(self, flip:bool) -> None:
+        #true = Flip, False = BOW
+        if not isinstance(flip,bool):
+            raise ValueError('crash_mode must be a bool') 
+        self._crash_mode = flip  
 
+    @property
+    def mod_rate(self) -> int:
+        return self._mod_rate
+    
+    @mod_rate.setter
+    def mod_rate(self, mod_rate:int):
+        if not isinstance(mod_rate, int):
+            raise ValueError('mod_rate must be an int')
+        if mod_rate<0 or mod_rate>3:
+            raise ValueError('mod_rate must be in the range [0,3]')
+        self._mod_rate = mod_rate
+    
+    
     @property
     def mod_frequency(self) -> int:
         return self._mod_frequency
@@ -99,7 +94,7 @@ class Drive ():
     def mod_frequency(self, frequency:int) -> None:
         if not isinstance(frequency, int):
             raise ValueError('frequency must be an int')
-        if frequency<0 or frequency>15:
+        if frequency<0 or frequency>7:
             raise ValueError('frequency must be in the range [0,15]')
         self._mod_frequency = frequency
 
@@ -108,15 +103,14 @@ class Drive ():
         return self._frequency
     
     @frequency.setter
-    def frequency(self, frequency:float) -> None:
-        if not isinstance(frequency, float):
-            raise ValueError('frequency must be float')
-        self._frequency = round(frequency,3)
+    def frequency(self, frequency) -> None:
+        self._frequency = round(float(frequency),3)
 
     def update(self, just_CTRL:bool=False) -> None:
-
-        CTRL = (self.mod_frequency << 4)
-        CTRL = CTRL |(self.crash_mode << 2)     
+        
+        CTRL = (self.mod_rate << 6)
+        CTRL = CTRL | (self.mod_frequency << 3)
+        CTRL = CTRL | (self.flip << 2)     
         CTRL = CTRL | (self.spin << 1)
         CTRL = CTRL | (self.enable)
 
@@ -134,17 +128,77 @@ class Drive ():
         print(f"array length: {len(FREQ)}")       
 
 
-        if just_CTRL:
-            bus.write(self.address, CTRL, [])
-        else:
-            bus.write(self.address, CTRL, FREQ)
-
+        # if just_CTRL:
+        #     bus.write(self.address, CTRL, [])
+        # else:
+        #     bus.write(self.address, CTRL, FREQ)
 
 
 pp = Drive(8)
 
+pp.enable = pp.spin = pp.flip = True
+
+pp.mod_rate = 3
+pp.mod_frequency = 0
 
 
 pp.update()
+
+
+
+exit(0)
+
+
+
+pp = Drive(8)
+try:
+    pp.enable = True
+    pp.mod_frequency = 0
+    pp.frequency = 440.0
+    pp.update(just_CTRL=False)
+    print("no mod")
+    time.sleep(2)
+
+    # pp.frequency = 466.0
+    # pp.update(just_CTRL=False)
+    # print("no mod")
+    # time.sleep(2)
+
+
+    # pp.frequency = 493.0
+    # pp.update(just_CTRL=False)
+    # print("no mod")
+    # time.sleep(2)
+
+
+    # pp.frequency = 523.0
+    # pp.update(just_CTRL=False)
+    # print("no mod")
+    # time.sleep(2)
+
+
+
+    # pp.mod_frequency = 2
+    # pp.update(False)
+    # print("mod 2")
+    # time.sleep(1)
+    for i in range(1,16):
+        print('mod', i)
+        pp.mod_frequency = i
+        pp.update(just_CTRL=True)
+        time.sleep(5)
+
+    pp.mod_frequency =0
+    pp.update(just_CTRL=True)
+    print("no mod")
+    time.sleep(5)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    pp.enable =False
+    pp.mod_frequency = 0
+    pp.frequency = 440.0
+    pp.update(False)
 
 
